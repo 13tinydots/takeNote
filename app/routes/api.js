172a4f170c3ebf3695eb437bbe229dc721d61c60
13tinydots <13tinydots@gmail.com>
2021-07-28 17:27:55 -0500

    updated routing and db service

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import db from "../db/service.js";
+
 const router = new Router();
 router.get("/notes", async (req, res) => {
   const notes = await db.index();
   res.status(200).json(notes);
 });
+
 router.post("/notes", ({ body }, res) => {
   db.create(body);
-  res.status(201).send("Note Added!");
 });
 export default router;
