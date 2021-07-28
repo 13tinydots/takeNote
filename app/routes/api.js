637e53223438e776d734558ba32b0733c7bb5b79
13tinydots <13tinydots@gmail.com>
2021-07-28 18:22:03 -0500

    commenting up index.js

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import db from "../db/service.js";
+const realPath = null;
 
+// Define the API routes
 const router = new Router();
 router.get("/notes", async (req, res) => {
   const notes = await db.index();
-  res.status(200).json(notes);
+  // make sure to return status code 200
+  return res.status(200).json(notes);
 });
 
+// Post method for a note
 router.post("/notes", ({ body }, res) => {
   db.create(body);
 });
