922299899a238ac6a3dd8a794afcf5e65dae35b3
13tinydots <13tinydots@gmail.com>
2021-07-28 20:53:17 -0500

    updated port

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@
 
 import express from "express";
 import apiRouter from "./routes/api.js";
-const PORT = 3001;
+const PORT = 3000;
 const app = express();
 
 // telling express to use the public folder
